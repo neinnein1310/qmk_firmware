67c0a271416e904e3f1df8c7127b17d4bea8496c
HorrorTroll <sonicvipduc@gmail.com>
2021-12-08 12:39:29 +0700

    Resolved issue of keyboard layout thingy

--- keyboards/gopolar/gg86/gg86.h
+++ keyboards/gopolar/gg86/gg86.h
@@ -35,7 +35,7 @@
  *             └─────┴───┴─────┴───────────────────────────┴─────┴───┴─────┘ └───┴───┴───┘
  */
 
-#define LAYOUT_tkl_multi_7u( \
+#define LAYOUT_tkl_ansi_tsangan_split_lrshift( \
     K00,      K01, K02, K03, K04,   K05, K06, K07, K08,    K09, K0A, K0B, K0C,  K0E, K0F, K0G, \
     K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, K1C,    K1D,    K1E, K1F, K1G, \
      K20,  K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B, K2C,   K2D,   K2E, K2F, K2G, \
